88af66bd1271661c4202c3570009912f0b1a5d06
mingmoth <mingmoth@gmail.com>
2022-01-16 14:09:27 +0800

    add:Navbar layout & css

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,19 @@
   <div>
     <nav>
       <div class="nav-container">
-        <img src="../assets/logo_dark.png" alt="">
+        <img src="../assets/logo_dark.png" alt="" class="nav-logo">
+        <div class="search-container">
+            <input type="text" name="search" id="search">
+            <button>
+              <img src="../assets/search_icon.png" alt="" class="search-icon">
+            </button>
+          </div>
+        <div class="nav-options">
+          <img src="../assets/setting logo.png" alt="" class="admin-icon">
+          <img src="../assets/cart_icon.png" alt="" class="cart-icon">
+          <img src="../assets/user logo.png" alt="">
+          <img src="../assets/moon_icon.png" alt="" class="theme-toggle-icon">
+        </div>
       </div>
     </nav>
   </div>
