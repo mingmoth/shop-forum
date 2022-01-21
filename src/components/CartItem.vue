b2c05bbbfa03aad31643d4c6105531b128b56f31
mingmoth <mingmoth@gmail.com>
2022-01-21 11:11:42 +0800

    add: CartItem.vue & css

--- src/components/CartItem.vue
+++ src/components/CartItem.vue
@@ -1,5 +1,22 @@
 <template>
-  <div></div>
+  <div class="cart">
+    <div class="cart-head">
+      <router-link to="/products">
+      <img src="../assets/product_detail.jpeg" alt="" class="cart-image"></router-link>
+      <div></div>
+    </div>
+    <div class="cart-body">
+      <div class="cart-sum">
+        <div class="cart-name">力宏Wanggggggggg</div>
+        <div class="cart-count">
+          <img src="../assets/minus_light.png" alt="" class="cart-minus">
+          <div class="cart-quantity">1</div>
+          <img src="../assets/plus_light.png" alt="" class="cart-plus">
+        </div>
+      </div>
+      <div class="cart-price"><span>$ </span>律師費</div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,3 +24,7 @@ export default {
   name: 'CartItem',
 }
 </script>
+
+<style lang="sass" scoped>
+@import '../styles/_cart'
+</style>
--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -4,17 +4,44 @@
       <Navbar />
     </div>
     <div class="main">
-      <CartItem />
+      <div class="cart-container">
+        <p>購物車列表</p>
+        <div class="cart-wrapper">
+          <CartItem />
+          <CartItem />
+          <CartItem />
+          <CartItem />
+        </div>
+      </div>
     </div>
     <div class="foot"></div>
   </div>
 </template>
 
 <script>
-import Navbar from '../components/Navbar.vue'
-import CartItem from '../components/CartItem.vue'
+import Navbar from "../components/Navbar.vue";
+import CartItem from "../components/CartItem.vue";
 export default {
-  name: 'Cart',
-  components: { Navbar, CartItem }
-}
+  name: "Cart",
+  components: { Navbar, CartItem },
+};
 </script>
+
+<style lang="sass" scoped>
+.cart-container
+  max-width: 1280px
+  margin: 2% auto
+  padding: 2%
+  border: 1px solid $border-grey
+  border-radius: 8px
+  font-size: 1.3em
+  font-weight: 500
+  p
+    border-bottom: 1px solid $border-grey
+    padding-bottom: 1em
+  .cart-wrapper
+    max-height: 600px
+    overflow-y: auto
+    &::-webkit-scrollbar
+      display: none
+</style>
