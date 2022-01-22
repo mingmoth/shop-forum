335dac713542fd5afeef2552f9f1ca3ad9590b83
mingmoth <mingmoth@gmail.com>
2022-01-22 15:14:24 +0800

    add: vuex & add module files

---
+++ src/store/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import authorization from './modules/authorization'
+import user from './modules/user'
+import order from './modules/order'
+import product from './modules/product'
+import cartItem from './modules/cartItem'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+    authorization,
+    user,
+    order,
+    product,
+    cartItem
+  }
+})
